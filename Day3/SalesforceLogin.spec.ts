10d5cbdd36ebd3b1e95805aaeda417bee9ae1c4b
mdfayas <47477189+mdfayas@users.noreply.github.com>
2023-12-03 14:38:58 +0530

    Update

--- Day3/SalesforceLogin.spec.ts
+++ Day3/SalesforceLogin.spec.ts
@@ -1,5 +1,5 @@
 import {chromium, test} from "@playwright/test";
-test("To lauch Browser",async()=>{
+test("Login Salesforce",async()=>{
     const browserInstance = await chromium.launch({headless: false, channel: "chrome"});
     const browserContext = await browserInstance.newContext();
     const page = await browserContext.newPage();
